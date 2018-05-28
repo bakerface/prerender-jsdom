5104a0a58f0363ef3fb7e28e0fba62f2551a8b74
Chris Baker <mail.chris.baker@gmail.com>
2018-05-28 03:40:34 +0000

    v1.0.0
    Added cli.js

---
+++ README.md
@@ -0,0 +1,12 @@
+# prerender-jsdom
+**Prerender static routes using jsdom**
+
+Just `npm install prerender-jsdom` and add the following to your package.json:
+
+``` json
+{
+  "scripts": {
+    "postbuild": "prerender dist /"
+  }
+}
+```
---
+++ cli.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const mkdirp = require('mkdirp');
+const Prerenderer = require('@prerenderer/prerenderer');
+const Renderer = require('@prerenderer/renderer-jsdom');
+
+const createDir = util.promisify(mkdirp);
+const writeFile = util.promisify(fs.writeFile);
+const staticDir = path.join(__dirname, process.argv[2]);
+const routes = process.argv.slice(3);
+
+const prerenderer = new Prerenderer({
+  staticDir,
+  renderer: new Renderer({
+    renderAfterElementExists: '#app',
+  }),
+});
+
+async function save(rendered) {
+  const dir = path.join(staticDir, rendered.route);
+  const file = path.join(dir, 'index.html');
+
+  await createDir(dir);
+  await writeFile(file, rendered.html.trim());
+}
+
+async function prerender() {
+  await prerenderer.initialize();
+  const rendered = await prerenderer.renderRoutes(routes);
+  await Promise.all(rendered.map(save));
+}
+
+prerender().then(() => process.exit());
